46fde3eae643aee32dc5edc61fcf4a99faa3c792
binup5727 <binup5727@gmail.com>
2022-03-10 14:29:08 -0500

    init repo

---
+++ src/components/Coaches.js
@@ -0,0 +1,31 @@
+
+
+function Coaches(){
+
+    return <div> 
+            <ol>
+                
+            <li>Vinny</li>
+            <li>Vinny</li>
+            <li>Vinny</li>    
+                
+            </ol>
+
+    </div>
+
+
+
+
+
+
+}
+
+
+export default Coaches
+
+
+
+
+
+
+
